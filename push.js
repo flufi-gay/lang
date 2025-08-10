d5a102d7bd26a94f985775e54da65d88d09e0681
Flufi-boi <floofster@flufi.gay>
2025-08-10 17:44:36 +0100

    update (no name given :P

---
+++ push.js
@@ -0,0 +1,13 @@
+import { execSync } from 'child_process';
+
+const message = process.argv[2] ?? "update (no name given :P";
+
+
+try {
+    execSync('git add .', { stdio: 'inherit' });
+    execSync(`git commit -m "${message}"`, { stdio: 'inherit' });
+    execSync('git push -u origin master', { stdio: 'inherit' });
+} catch (error) {
+    console.error('sad', error.message);
+    process.exit(1);
+}
